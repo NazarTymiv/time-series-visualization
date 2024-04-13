335f0f2c432f48eb9cc92dcd681f6a00b7515e88
Nazar Tymiv <nazartumiv@gmail.com>
2024-04-13 16:46:36 +0100

    feat: install godotenv and use port variable into main function

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Printf("Hello World!")
+
+	godotenv.Load()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("\nPort:", port)
 }
