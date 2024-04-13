b0ebbfaff351ccfe4264a11522532656b3683e60
Nazar Tymiv <nazartumiv@gmail.com>
2024-04-13 17:01:02 +0100

    feat: add cors configuration

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -21,13 +22,24 @@ func main() {
 
 	router := chi.NewRouter()
 
+	// cors-configuration
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	// server-configuration
 	server := http.Server{
 		Handler: router,
 		Addr: ":"+port,
 	}
 
 	fmt.Printf("Server has been running on port %v", port)
-	
+
 	err := server.ListenAndServe()
 
 	if err != nil {
