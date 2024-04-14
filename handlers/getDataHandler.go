aef30c7224c36d996993178e961085191dd41b05
Nazar Tymiv <nazartumiv@gmail.com>
2024-04-15 00:52:33 +0100

    feat: add counted delta to getDataHandler function

--- handlers/getDataHandler.go
+++ handlers/getDataHandler.go
@@ -6,8 +6,21 @@ import (
 	"github.com/NazarTymiv/time-series-visualization/helpers"
 )
 
+type Data struct {
+	Points []helpers.DataPoint `json:"points"`
+	Delta float64 `json:"delta"`
+}
+
 func GetDataHandler(w http.ResponseWriter, r *http.Request) {
-	data := helpers.GenerateTimeSeries(10)
+	dataPoints := helpers.GenerateTimeSeries(10)
+
+	startValue := dataPoints[0].Value
+	endValue := dataPoints[len(dataPoints) - 1].Value
+
+	data := Data{
+		Points: dataPoints,
+		Delta: endValue - startValue,
+	}
 
 	helpers.RespondWithJSON(w, 200, data)
 }
